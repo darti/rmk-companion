78a6ff0b0635b2a1545536e65149360ca83599f9
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2022-04-17 21:39:09 +0200

    Add main loop

--- rmk-daemon/src/bin/mount.rs
+++ rmk-daemon/src/bin/mount.rs
@@ -1,10 +1,54 @@
+use std::sync::Arc;
+
 use anyhow::Result;
+use futures::stream::StreamExt;
 use log::info;
+use signal_hook::consts::signal::*;
+use signal_hook_tokio::Signals;
+use tokio::sync::Mutex;
+
+async fn handle_signals(mut signals: Signals, exit_flag: Arc<Mutex<bool>>) {
+    while let Some(signal) = signals.next().await {
+        info!("Signal: {}", signal);
+
+        let mut lock = exit_flag.lock().await;
+        *lock = true;
+
+        // match signal {
+        //     SIGHUP => {
+        //         // Reload configuration
+        //         // Reopen the log file
+        //     }
+        //     SIGTERM | SIGINT | SIGQUIT => {
+        //         // Shutdown the system;
+        //     }
+        //     _ => unreachable!(),
+        // }
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init();
     info!("Starting rmk-daemon");
+
+    let exit_flag = Arc::new(Mutex::new(false));
+
+    let signals = Signals::new(&[SIGHUP, SIGTERM, SIGINT, SIGQUIT])?;
+    let handle = signals.handle();
+
+    let signals_task = tokio::spawn(handle_signals(signals, exit_flag.clone()));
+
+    loop {
+        if *exit_flag.lock().await {
+            break;
+        }
+    }
+
+    info!("Shutting down rmk-daemon");
+
+    handle.close();
+    signals_task.await?;
 
     Ok(())
 }
