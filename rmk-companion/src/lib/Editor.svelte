689e0cd3596d5dbb8b99fbe161db4c185a92a626
Matthieu Dartiguenave <matthieu.dartiguenave@gmail.com>
2022-07-10 10:49:02 +0100

    Add monaco editor to client

---
+++ rmk-companion/src/lib/Editor.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { onMount } from 'svelte';
+
+  import * as monaco from 'monaco-editor';
+  import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+
+  let container;
+
+  onMount(() => {
+    monaco.editor.create(container, {
+      value: 'SELECT * FROM narf',
+      language: 'sql',
+    });
+  });
+</script>
+
+<div
+  class="monaco-container"
+  bind:this={container}
+  style="height: 100%;  widht:100%; text-align: left"
+/>
